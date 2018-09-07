37d6adfe06ca4bf76f2748cce8b9113a2f980b5a
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2018-09-07 11:22:41 +0000

    photos table

--- ig_clone.sql
+++ ig_clone.sql
@@ -9,7 +9,31 @@ CREATE TABLE users
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE photos 
+(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL, 
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+
 INSERT INTO users (username) VALUES
 ('BlueTheCat'),
 ('CharlieBrown'),
 ('ColtSteele');
+
+INSERT INTO photos (image_url, user_id) VALUES
+('/sadfasfasf', 1),
+('/iyuiuyiy', 2),
+('/vxcbxcaqqweoi', 2);
+
+
+
+
+---------------------------------------------------------
+SELECT photos.image_url, users.username
+FROM photos
+JOIN users
+    ON photos.user_id = users.id;
